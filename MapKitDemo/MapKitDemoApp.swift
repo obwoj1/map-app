7c23794d015ba8f37b51c05d83f103db9215cf5a
obwoj1 <149099282+obwoj1@users.noreply.github.com>
2024-02-27 20:19:58 -0500

    Initial Commit

---
+++ MapKitDemo/MapKitDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapKitDemoApp.swift
+//  MapKitDemo
+//
+//  Created by Oba W on 2/27/24.
+//
+
+import SwiftUI
+
+@main
+struct MapKitDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
